eff6a46142b65ee61fcd283a2a7561b69c70e33b
btarbazz <btarbazz@gmail.com>
2022-08-08 02:20:14 +0500

    updated on 08082022

---
+++ src/routes/navigation-bar/navigation-bar.jsx
@@ -0,0 +1,29 @@
+import { Outlet, Link } from 'react-router-dom';
+import { Fragment } from 'react';
+import { ReactComponent as AkLogo } from '../../assets/ak.svg';
+import './navigation-bar.styles.scss';
+
+const NavigationBar = () => {
+  return (
+    <Fragment>
+      <div className="nav-bar-container">
+        <Link className="logo-container" to="/">
+          <AkLogo className="logo" />
+        </Link>
+        <div className="nav-bar-links-container">
+          <Link className="nav-bar-link" to="shop">
+            Shop
+          </Link>
+          <Link className="nav-bar-link" to="shop">
+            Shop
+          </Link>
+        </div>
+      </div>
+      <div>
+        <Outlet />
+      </div>
+    </Fragment>
+  );
+};
+
+export default NavigationBar;
