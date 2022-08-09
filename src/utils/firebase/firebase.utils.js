73cd7bc9fdbda7f559eba3f83c6f7f3321ec9b83
btarbazz <btarbazz@gmail.com>
2022-08-09 12:44:31 +0500

    updated on 09082022

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -5,6 +5,8 @@ import {
   signInWithRedirect,
   signInWithPopup,
 } from 'firebase/auth';
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+// getFirestore = database , doc = instance of doc, getDoc = snapshot of ur doc, setDoc = update doc
 
 const firebaseConfig = {
   apiKey: 'AIzaSyCkxg6E6LlZD7azUR_vmk0jIWKGe_j4HSk',
@@ -18,10 +20,44 @@ const firebaseConfig = {
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
 
+// Initialize Provider(instance) and Auth
+
 const provider = new GoogleAuthProvider(); // we can have multiple provider
 provider.getCustomParameters({
   prompt: 'select-account', // only allow to select acc
 });
-
 export const auth = getAuth();
+
+// Sign In Functions
+
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+// FireStore
+
+export const db = getFirestore();
+
+export const createUserDocFromAuth = async userAuth => {
+  const userDocRef = doc(db, 'users', userAuth.uid);
+
+  const userDocSnapshot = await getDoc(userDocRef);
+
+  /* if user does not exist
+  user create karna hai
+  if exist
+  give back user doc ref */
+  if (!userDocSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log('There is a error in users', error.message);
+    }
+  }
+  return userDocRef;
+};
