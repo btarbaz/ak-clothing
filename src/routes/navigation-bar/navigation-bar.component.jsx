4d7fa31569ad45ef3f245d190b75b97c64f22fa3
lappichu <btarbazz@gmail.com>
2022-08-16 16:26:18 +0500

    updated on 16082022

--- src/routes/navigation-bar/navigation-bar.component.jsx
+++ src/routes/navigation-bar/navigation-bar.component.jsx
@@ -1,12 +1,20 @@
 import { Outlet, Link } from 'react-router-dom';
 import { Fragment, useContext } from 'react';
+
 import { UserContext } from '../../contexts/user.context';
+import { CartContext } from '../../contexts/cart.context';
+
 import { signOutUser } from '../../utils/firebase/firebase.utils';
+
+import CartIcon from '../../components/cart-icon/cart-icon.component';
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
+
 import { ReactComponent as AkLogo } from '../../assets/ak.svg';
 import './navigation-bar.styles.scss';
 
 const NavigationBar = () => {
   const { currentUser } = useContext(UserContext);
+  const { isCartOpen } = useContext(CartContext);
   const signOutHandler = async () => {
     await signOutUser();
   };
@@ -30,8 +38,10 @@ const NavigationBar = () => {
               SIGN IN
             </Link>
           )}
+          <CartIcon />
         </div>
       </div>
+      {isCartOpen && <CartDropdown />}
       <div>
         <Outlet />
       </div>
